26a17dc9652b54419a36b64869539c2c65a2698a
Tomasz Wasik <tomaszwasik31@gmail.com>
2022-09-29 15:54:49 +0100

    live version 'bug'

--- src/menu.js
+++ src/menu.js
@@ -1,23 +1,23 @@
-const menuPage = () => {
-  console.log("change");
+// const menuPage = () => {
+//   console.log("change");
 
-  const createHeading = () => {
-    const div = document.createElement("div");
-    const h1 = document.createElement("h1");
+//   const createHeading = () => {
+//     const div = document.createElement("div");
+//     const h1 = document.createElement("h1");
 
-    h1.innerText = "pizza";
-    content.appendChild(div);
-    div.appendChild(h1);
-  };
+//     h1.innerText = "pizza";
+//     content.appendChild(div);
+//     div.appendChild(h1);
+//   };
 
-  // class Pizza {
-  //   constructor(name, price) {
-  //     this.name = name;
-  //     this.price = price;
-  //   }
-  // }
-  // const margarita = new Pizza("Margarita ", 10);
-  // createHeading();
-};
+//   class Pizza {
+//     constructor(name, price) {
+//       this.name = name;
+//       this.price = price;
+//     }
+//   }
+//   const margarita = new Pizza("Margarita ", 10);
+//   createHeading();
+// };
 
-export default menuPage;
+// export default menuPage;
